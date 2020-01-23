c5a64768a0c787f36766424878c2a36cbd0646b0
Nabin <echonabin@gmail.com>
2020-01-23 07:35:11 +0545

    api added

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+class HelloApiView(APIView):
+    """ Test API """
+    def get(self, request, format=None):
+        """ API view features """
+        an_APIview = [
+            'just an api view with i dont understand a single freaking thing!'
+            'i guess this is just a causual text rather than a code'
+        ]
+
+        return Response({'message' : 'Hi!', 'apiview' : an_APIview})
